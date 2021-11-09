2a4752e2686e9bbb5b3baad3bdb3ef20c72c0f85
Esteban Casalotti <estebancasalotti2@gmail.com>
2021-11-09 16:55:04 -0300

    cambios routes

--- src/App.js
+++ src/App.js
@@ -5,34 +5,44 @@ import {List} from './components/List'
 import {Elementos, ItemListContainer} from './components/ItemListContainer'
 import {Counter} from './components/ItemCount'
 import {ItemDetailConteiner} from './components/ItemDetailConteiner';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { ItemDetail } from './components/ItemDetail';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Title />
-        <List />
-        <ItemListContainer>
-          <h2> Bienvenidos! </h2>
-        </ItemListContainer>
-        <Counter />
-        <Elementos />
-        <ItemDetailConteiner />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/">
+          <ItemDetail />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
+    // <div className="App">
+    //   <header className="App-header">
+    //     <Title />
+    //     <List />
+    //     <ItemListContainer>
+    //       <h2> Bienvenidos! </h2>
+    //     </ItemListContainer>
+    //     <Counter />
+    //     <Elementos />
+    //     <ItemDetailConteiner />
 
 
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
+    //     {/* <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a> */}
+    //   </header>
+    // </div>
   
   );
 }
